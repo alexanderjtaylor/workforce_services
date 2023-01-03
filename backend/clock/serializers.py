8cf5a1cf85485d936d08f2bfcb57117fe708c578
Alex <alexandertaylor315@gmail.com>
2023-01-03 02:18:27 -0500

    added shift_id to clock serializer

--- backend/clock/serializers.py
+++ backend/clock/serializers.py
@@ -4,5 +4,5 @@ from .models import Clock
 class ClockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clock
-        fields = ['id', 'clockIn', 'clockOut', 'startLunch', 'returnLunch', 'employee_id']
+        fields = ['id', 'clockIn', 'clockOut', 'startLunch', 'returnLunch', 'employee_id', 'shift_id']
         depth = 1
