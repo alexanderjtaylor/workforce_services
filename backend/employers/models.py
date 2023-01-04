bc8b60c5b5cac73ef11f21585596a230c9e236b9
Alex <alexandertaylor315@gmail.com>
2023-01-03 21:47:23 -0500

    updated apps.py files for all created apps to config + in process of allowing numberofemployees execute a count of employees for each employer

--- backend/employers/models.py
+++ backend/employers/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from authentication.models import User
+from employees.models import Employee
 
 class Employer(models.Model):
     companyName = models.CharField(max_length=255)
     numberOfEmployees = models.IntegerField()
+    
+    def save(self, *args, **kwargs):
+        employee_count = Employee.objects.filter(employer=self).count()
+        self.numberOfEmployees = employee_count
+        super(Employer, self).save(*args, **kwargs)
