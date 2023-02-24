ef247caac64179afaca794988df91cf8c7ff65da
Alex <alexandertaylor315@gmail.com>
2023-02-23 22:56:47 -0500

    filtering through time punches for the week and attempting to get back hoursworked, overtime hours worked, sick time used, vacation time used to use for axios post request

--- frontend/src/pages/PaycheckHomePage/CreatePaycheckPage.jsx
+++ frontend/src/pages/PaycheckHomePage/CreatePaycheckPage.jsx
@@ -11,10 +11,117 @@ const CreatePayCheckPage = (props) => {
     const navigate = useNavigate();
     const [formData, handleInputChange, handleSubmit] = useCustomForm(state, postNewPaycheck);
     const {employeeID} = useParams();
-    const [thisEmployee, setThisEmployee] = useState({});
+    const [punches, setPunches] = useState([]);
+    const [punchValues, setPunchValues] = useState([]);
     console.log(state)
 
+    const moment = require('moment');
+    let dayOfWeek = moment().day();
+    let numDay = moment().date(); 
+    let start = new Date();
+    let startWeekTitle = new Date();
+    start.setDate((numDay - dayOfWeek) - 1);
+    startWeekTitle.setDate((numDay - dayOfWeek));
+    start.setHours(0, 0, 0, 0);
+    let startOfWeek = moment(start).format("MM/DD/YYYY");
+    let startOfWeekTitle = moment(startWeekTitle).format("MM/DD/YYYY");
+    let end = new Date();
+    let endWeekTitle = new Date();
+    end.setDate(numDay + (7 - dayOfWeek));
+    endWeekTitle.setDate(numDay + (6 - dayOfWeek));
+    end.setHours(0, 0, 0, 0);
+    let endOfWeek = moment(end).format("MM/DD/YYYY");
+    let endOfWeekTitle = moment(endWeekTitle).format("MM/DD/YYYY");
+    const startDate = moment(startOfWeek, "MM/DD/YYYY");  
+    const endDate = moment(endOfWeek, "MM/DD/YYYY");  
+    let values = ""
+    let clockIn = ""
+    let clockOut = ""
+    let startLunch = ""
+    let returnLunch = ""
+    let sickTimeUsed = ""
+    let vacationTimeUsed = ""
+    let OTHoursWorked = ""
+    let hoursWorked = ""
+    let taxes = ""
+
+    useEffect(() => {
+        fetchPunches();
+      }, [token]);
+
+    const fetchPunches = async () => {
+        try {
+          let response = await axios.get(`http://127.0.0.1:8000/clock-in/${state.employee_id}/punches`, {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          });
+        //   setPunches(todaysPunch(response.data));
+        //   console.log(todaysPunch(response.data))
+        console.log(thisWeeksPunches(response.data))
+        setPunches(thisWeeksPunches(response.data))
+        // let values = thisWeeksPunches(response.data).reduce(function(previousValue, currentValue){
+        //     return {
+        //         clockIn: previousValue.clockIn + currentValue.clockIn,
+        //         clockOut: previousValue.clockOut + currentValue.clockOut,
+        //         startLunch: previousValue.startLunch + currentValue.startLunch,
+        //         returnLunch: previousValue.returnLunch + currentValue.returnLunch,
+        //         sickTimeUsed: previousValue.shift.sickTimeUsed + currentValue.shift.sickTimeUsed,
+        //         vacationTimeUsed: previousValue.shift.vacationTimeUsed + currentValue.shift.vacationTimeUsed,
+        //     }
+        //   });
+        //   console.log(values);
+        //   console.log(clockIn);
+          // setPunches(response.data);
+        } catch (error) {
+          // console.log(response.data)
+        }
+      };
+
+      function thisWeeksPunches(punches) {
+        return punches.filter(punch => moment(punch.shift.workDate).isBetween(startDate, endDate));}
+ 
+        //   var newValues = punches.reduce(function(previousValue, currentValue) {
+        //     return {
+        //         clockIn: previousValue.clockIn + currentValue.clockIn,
+        //         clockOut: previousValue.clockOut + currentValue.clockOut,
+        //         startLunch: previousValue.startLunch + currentValue.startLunch,
+        //         returnLunch: previousValue.returnLunch + currentValue.returnLunch,
+        //         sickTimeUsed: previousValue.shift.sickTimeUsed + currentValue.shift.sickTimeUsed,
+        //         vacationTimeUsed: previousValue.shift.vacationTimeUsed + currentValue.shift.vacationTimeUsed,
+        //     }
+        //   });
+        //   console.log(newValues);
+
+          function paycheckNumbers(values) {
+            if(values.clockOut - values.clockIn + (values.returnLunch - values.startLunch) >= 40){
+                return{
+                OTHoursWorked: (40 - (values.clockOut - values.clockIn + (values.startLunch - values.returnLunch))),
+                hoursWorked: (OTHoursWorked - (values.clockOut - values.clockIn + (values.startLunch - values.returnLunch))),
+                sickTimeUsed: values.sickTimeUsed,
+                vacationTimeUsed: values.vacationTimeUsed,
+                taxes: .20}}
+            else{
+                return{
+                OTHoursWorked: 0,
+                hoursWorked: (values.clockOut - values.clockIn + (values.startLunch - values.returnLunch)),
+                sickTimeUsed: (values.sickTimeUsed ? sickTimeUsed: 0),
+                vacationTimeUsed: (values.vacationTimeUsed ? vacationTimeUsed: 0),
+                taxes: .20}}};
+
+            paycheckNumbers(punches)
+            console.log(paycheckNumbers(punches))
+
+            // const totalHours = punches.map(clock => clockOut.clockOut - clock.clockIn + (values.startLunch - values.returnLunch));
+            // console.log(totalHours);
+
+
     async function postNewPaycheck(){
+        formData["hoursWorked"] = hoursWorked
+        formData["OTHoursWorked"] = OTHoursWorked
+        formData["sickTimeUsed"] = sickTimeUsed
+        formData["vacationTimeUsed"] = vacationTimeUsed
+        formData["taxes"] = taxes
         try {
             let response = await axios.post("http://127.0.0.1:8000/paychecks/create-paycheck/", formData, {
                 headers: {
@@ -44,23 +151,23 @@ const CreatePayCheckPage = (props) => {
             </label>
             <label>
                 Base Pay Hours:{" "}
-                <input type="text" name="hoursWorked" value={formData.hoursWorked} onChange={handleInputChange}/>
+                <input type="text" name="hoursWorked" value={hoursWorked} onChange={handleInputChange}/>
             </label>
             <label>
                 Overtime Hours:{" "}
-                <input type="text" name="OTHoursWorked" value={formData.OTHoursWorked} onChange={handleInputChange}/>
+                <input type="text" name="OTHoursWorked" value={OTHoursWorked} onChange={handleInputChange}/>
             </label>
             <label>
                 Sick Time:{" "}
-                <input type="text" name="sickTimeUsed" value={formData.sickTimeUsed} onChange={handleInputChange}/>
+                <input type="text" name="sickTimeUsed" value={sickTimeUsed} onChange={handleInputChange}/>
             </label>
             <label>
                 Vacation Time:{" "}
-                <input type="text" name="vacationTimeUsed" value={formData.vacationTimeUsed} onChange={handleInputChange}/>
+                <input type="text" name="vacationTimeUsed" value={vacationTimeUsed} onChange={handleInputChange}/>
             </label>
             <label>
                 Taxes:{" "}
-                <input type="text" name="taxes" value={formData.taxes} onChange={handleInputChange}/>
+                <input type="text" name="taxes" value={taxes} onChange={handleInputChange}/>
             </label>
             <button className='add-paycheck-btn'>Issue Paycheck</button>
         </form>
